4413c612314ec432e8a92b783fbf78127b949647
René van Balen <rene.vanbalen@gmail.com>
2022-03-04 15:03:32 +0100

    Add NDES installation script

---
+++ NDES Setup/Install-NDESServer.ps1
@@ -0,0 +1,17 @@
+# Add NDES Service Account: svc_ndes@domain.local
+
+# Add svc_ndes to local Administrators and IIS_IUSRS groups
+
+# Create _KLANT Intune NDES Authentication certificate template
+# Client Authentication, Smartcard Logon
+
+# Create _KLANT Intune NDES Server certificate
+# Client- + Server-authentication
+
+# Install AD CS NDES Role and additional WebServer features
+# Check if Sources\SxS folder is available
+
+# setspn -s http/<DNS name of the computer that hosts the NDES service> <Domain name>\<NDES Service account name>
+
+# Configure NDES service
+
