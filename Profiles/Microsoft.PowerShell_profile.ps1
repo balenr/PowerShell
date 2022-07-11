06a47740fd8f7b332a4dcbec9aaa662f2b02029b
René van Balen <rene.vanbalen@gmail.com>
2022-07-12 00:25:10 +0200

    :arrow_up: Update PowerShell Profile

--- Profiles/Microsoft.PowerShell_profile.ps1
+++ Profiles/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,1 @@
-$esc = "$([char]27)"
-$heart = "$([char]0xf004)"
-$fore = "$esc[38;5"
-$back = "$esc[48;5"
-$reset = "$esc[0m"
-
-function Prompt { "I $esc[31m$heart$esc[0m" + " PS $fore;249m$(Split-Path -Leaf -Path (Get-Location))$reset$('>' * ($NestedPromptLevel + 1)) " }
+if (Test-Path -Path $env:HOME/.config/powershell/user_profile.ps1) { . $env:HOME/.config/powershell/user_profile.ps1 }
--- Profiles/README.md
+++ Profiles/README.md
@@ -1,2 +1,18 @@
 # PowerShell Profiles
+
 My PowerShell Profiles
+
+## Setup
+
+Install ohmyposh.dev
+
+```shell
+mkdir -p ~/.config/powershell
+cp user_profile.ps1 rene.omp.json ~/.config/powershell
+```
+
+Edit $PROFILE
+
+```shell
+. ~/.config/powershell/user_profile.ps1
+```
